a12667aef5c4ffca605d1876064610cd32cc37d3
Abv191 <abvabv191@gmail.com>
2024-02-18 22:00:58 +0200

    Installation as a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Bot',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
