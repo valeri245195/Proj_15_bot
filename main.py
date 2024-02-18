58f1f247be70c468dac08af7266d66c0e64ef522
AndrewUAV <145654146+AndrewUAV@users.noreply.github.com>
2024-02-18 14:42:33 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+"""
+"""
+def add_name():
+    pass
+
+def add_address():
+    pass
+
+def add_phone():
+    pass
+
+def add_email():
+    pass
+
+def add_birthday():
+    pass
+
+def days_to_birthday():
+    pass
+
+def is_valid_phone():
+    pass
+
+def is_valid_email():
+    pass
+
+def search_contacts():
+    pass
+
+def change():
+    pass
+
+def delete():
+    pass
+
+def add_notes():
+    pass
+
+def search_notes():
+    pass
+
+def change_notes():
+    pass
+
+def delete_notes():
+    pass
+
+def add_tags():
+    pass
+
+def search_tags():
+    pass
+
+def sort_tags():
+    pass
+
+def sort_files_in_path():
+    pass
